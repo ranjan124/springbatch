f86d5fba1dbdc91347e9931076509666912b1e7a
trjena <ranjanjena124@gmail.com>
2017-07-02 18:11:58 +0200

    added start and stop controllers

--- src/main/java/com/silbury/springbatch/springbatch/configurations/SchedulerConfiguration.java
+++ src/main/java/com/silbury/springbatch/springbatch/configurations/SchedulerConfiguration.java
@@ -3,7 +3,7 @@ package com.silbury.springbatch.springbatch.configurations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.*;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 /**
  * Scheduler Configuration
@@ -22,27 +24,50 @@ import org.springframework.stereotype.Component;
 public class SchedulerConfiguration {
     private Logger logger = LoggerFactory.getLogger(SchedulerConfiguration.class);
 
-    @Autowired
-    private JobLauncher launcher;
+    private final JobLauncher launcher;
+
+    private final JobOperator jobOperator;
+
+    private final Job job;
 
     @Autowired
-    private Job job;
+    public SchedulerConfiguration(JobLauncher launcher, JobOperator jobOperator, Job job) {
+        this.launcher = launcher;
+        this.jobOperator = jobOperator;
+        this.job = job;
+    }
 
-    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void scheduler() {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
                 .toJobParameters();
         JobExecution execution;
         try {
-            // check if the job is already running
+            // check if the job is already running using job operator
             execution = launcher.run(job, jobParameters);
             // check for the status, can be put in while loop
-            // may not be needed there is job finish listener also
+            // may not be needed there is job completion notification listener also
             logger.info(" Status ", execution.getStatus());
         } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void startJob() {
+        scheduler();
+    }
+
+    public void stopJob()
+    {
+        try {
+            Set<Long> runningJobs = jobOperator.getRunningExecutions("importUserJob");
+            jobOperator.stop(runningJobs.iterator().next());
+        } catch (NoSuchJobException e) {
+            logger.info("no job found");
+        } catch (JobExecutionNotRunningException | NoSuchJobExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/silbury/springbatch/springbatch/controllers/BatchController.java
+++ src/main/java/com/silbury/springbatch/springbatch/controllers/BatchController.java
@@ -1,5 +1,7 @@
 package com.silbury.springbatch.springbatch.controllers;
 
+import com.silbury.springbatch.springbatch.configurations.SchedulerConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class BatchController {
+
+    @Autowired
+    private SchedulerConfiguration configuration;
+
     @RequestMapping("helloBatch")
     public ResponseEntity<String> helloBatch() {
         return new ResponseEntity<>("Hello Batch", HttpStatus.OK);
+    }
+
+    @RequestMapping("startBatch")
+    public ResponseEntity<?> startBatch()
+    {
+        configuration.startJob();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @RequestMapping("stopBatch")
+    public ResponseEntity<?> stopBatch()
+    {
+        configuration.stopJob();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
